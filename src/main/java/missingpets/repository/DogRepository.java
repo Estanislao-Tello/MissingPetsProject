4f0f3945ab8eee632d317ca62362fc565f236922
Estanislao-Tello <telloest09@gmail.com>
2022-05-20 19:44:03 -0300

    First commit

---
+++ src/main/java/missingpets/repository/DogRepository.java
@@ -0,0 +1,10 @@
+package missingpets.repository;
+
+import missingpets.entity.Dog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DogRepository extends JpaRepository<Dog, String>{
+    
+}
